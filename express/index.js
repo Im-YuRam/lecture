cf53a85813b7853ee59a2e365369b309c57e3ccb
Im-YuRam <aosuraimu913@gmail.com>
2026-06-26 18:15:01 +0900

    課題提出

--- express/index.js
+++ express/index.js
@@ -1,5 +1,48 @@
 const express =require("express");
 const app=express();
+const MAX=100
+app.use(express.json())
+let posts=[
+    {
+        id:1,
+        title:"First Post",
+        text:"First text"
+    }
+]
+let nextid=2
+app.post("/posts",(req,res)=>{
+    const{title}=req.body;
+    if (!title){
+        return res.status(400).json({error:"title is required"})
+    }
+    if (title.length>100){
+        return res.status(400).json({error:"title is so long"})
+    }
+    const post ={id:nextid++ ,title:req.body.title};
+    posts.push(post);
+    res.status(201).json(posts)
+})
+
+let posterior=[
+    {
+        id:1,
+        title:"First Post",
+        text:"First text"
+    }
+]
+app.post("/comments",(req,res)=>{
+    const{text}=req.body;
+    if (!text){
+        return res.status(400).json({error:"text is required"})
+    }
+    if (text.length>100){
+        return res.status(400).json({error:"text is so long"})
+    }
+    const post ={id:nextid++ ,text:req.body.text};
+    posterior.push(post);
+    res.status(201).json(posterior)
+})
+
 
 app.get("/",(req,res)=>{
     res.send("Hello Express")
