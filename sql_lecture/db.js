74d0ad0e2608173adc37b51f5fe6a37ef2485bbc
Im-YuRam <aosuraimu913@gmail.com>
2026-09-16 13:25:19 +0900

    第7回課題提出

--- sql_lecture/db.js
+++ sql_lecture/db.js
@@ -1,2 +1,16 @@
 const Database = require("better-sqlite3");
 const db = new Database("app.db");
+//dbが残ってしまうので、回数を重ねると投稿の数がずれます。気になるならdb消すなりidを取得してね
+db.exec(`
+    CREATE TABLE IF NOT EXISTS posts(
+    id INTEGER PRIMARY KEY,
+    title TEXT NOT NULL,
+    created_at TEXT
+    )
+`);
+
+db.prepare("INSERT INTO posts (title) VALUES (?)").run(" 初投稿 ");
+db.prepare("INSERT INTO posts (title) VALUES (?)").run(" 2投稿 ");
+db.prepare("INSERT INTO posts (title) VALUES (?)").run(" 3投稿 ");
+const rows = db.prepare("SELECT * FROM posts").all();
+console.log(rows);
